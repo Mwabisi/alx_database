b3a9d8359e0f5b1eab1ac67d1be4f82d404c36ba
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 20:59:55 +0200

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,12 @@
 -- Set the database to the specified one
 USE hbtn_0c_0;
 
--- Prepare a query to retrieve column information
-SELECT column_name, data_type, character_maximum_length
-FROM information_schema.columns
-WHERE table_name = 'first_table';
+-- Prepare a query to retrieve the table description
+SELECT CONCAT('first_table', CREATE_TABLE_STATEMENT)
+FROM (
+    SELECT
+        CONCAT('`', COLUMN_NAME, '`', COLUMN_TYPE, IF(IS_NULLABLE = 'NO', 'NOT NULL', 'DEFAULT NULL'), ',') AS CREATE_TABLE_STATEMENT
+    FROM information_schema.COLUMNS
+    WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
+    ORDER BY ORDINAL_POSITION
+) t;
