0484fd3a3487f3bafa493ddc4f046e1aca38e51f
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 21:39:11 +0200

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,13 +1,11 @@
 -- Check if the table exists
 IF (SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'first_table') = 0 THEN
   SELECT 'first_table' AS first_table,
-    CONCAT(
-        'CREATE TABLE `first_table` (',
-        '`id` int NOT NULL AUTO_INCREMENT,',
-        '`name` varchar(128) DEFAULT NULL,',
-        '`c` char(1) DEFAULT NULL,',
-        '`created_at` date DEFAULT NULL,',
-        'PRIMARY KEY (`id`)',
-        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
-    ) AS expected_output;
+    'CREATETABLE`first_table`(' ||
+    '`id`intNOTNULLAUTO_INCREMENT,' ||
+    '`name`varchar(128)DEFAULTNULL,' ||
+    '`c`char(1)DEFAULTNULL,' ||
+    '`created_at`dateDEFAULTNULL,' ||
+    'PRIMARYKEY(`id`)' ||
+    ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci' AS expected_output;
 END IF;
