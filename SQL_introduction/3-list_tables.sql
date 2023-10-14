35ab1a96d1b94600267a1c6010bf312bc7df2330
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 11:40:53 +0200

    List tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,6 +2,6 @@
 SET @db_name = '$1';
 
 -- Get a list of all the tables in the database
-SELECT table_name
+SELECT table_name AS Tables_in_hbtn_test_db_0
 FROM information_schema.tables
 WHERE table_schema = @db_name;
