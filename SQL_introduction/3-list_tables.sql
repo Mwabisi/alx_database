aa1854a7b63e9f918c8a72dceb15e8688f15225d
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 16:08:53 +0200

    List tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,8 @@
 -- Get the database name from the command line argument
-SET @db_name = '$1';
+SET @db_name = 'your_database_name';
 
 -- Get a list of all the tables in the database
-SELECT table_name AS Tables_in_hbtn_test_db_0
+SELECT table_name
 FROM information_schema.tables
 WHERE table_schema = @db_name;
+s
