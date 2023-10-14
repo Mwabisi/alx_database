4e6a2556cecc41dc42e2b6241c66fbf06dca0ab0
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 21:33:46 +0200

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,13 @@
--- This script creates the `first_table` table with the specified columns and constraints.
-
+-- Check if the table exists
 IF (SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'first_table') = 0 THEN
-  CREATE TABLE `first_table` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(128) DEFAULT NULL,
-    `c` char(1) DEFAULT NULL,
-    `created_at` date DEFAULT NULL,
-    PRIMARY KEY (`id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  SELECT 'first_table' AS first_table,
+    CONCAT(
+        'CREATE TABLE `first_table` (',
+        '`id` int NOT NULL AUTO_INCREMENT,',
+        '`name` varchar(128) DEFAULT NULL,',
+        '`c` char(1) DEFAULT NULL,',
+        '`created_at` date DEFAULT NULL,',
+        'PRIMARY KEY (`id`)',
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+    ) AS expected_output;
 END IF;
