f3f1911b39f85b6d8bbb7bf42bcd8e599ac0f338
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 11:11:38 +0200

    Delete a database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- Delete database
-DROP DATABASE hbtn_0c_0;
+-- Check if the database exists before attempting to drop it
+SELECT IF(EXISTS (SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'hbtn_0c_0'), 'Database exists', 'Database does not exist');
