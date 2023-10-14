b67119a68fe8cb9b90f760ac548732f8259719ac
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 21:04:49 +0200

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,15 @@
 -- Set the database to the specified one
 USE hbtn_0c_0;
 
--- Prepare a query to retrieve the table description
-SELECT CONCAT('first_table', CREATE_TABLE_STATEMENT)
-FROM (
-    SELECT
-        CONCAT('`', COLUMN_NAME, '`', COLUMN_TYPE, IF(IS_NULLABLE = 'NO', 'NOT NULL', 'DEFAULT NULL'), ',') AS CREATE_TABLE_STATEMENT
-    FROM information_schema.COLUMNS
-    WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
-    ORDER BY ORDINAL_POSITION
-) t;
+-- Prepare a query to retrieve and format the table description
+SELECT
+    CONCAT(
+        'first_table',
+        'CREATE TABLE `first_table` (',
+        '`id` int NOT NULL AUTO_INCREMENT,',
+        '`name` varchar(128) DEFAULT NULL,',
+        '`c` char(1) DEFAULT NULL,',
+        '`created_at` date DEFAULT NULL,',
+        'PRIMARY KEY (`id`)',
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+    );
