61bd8212ecda7b61b9a13fdb8ed037128653aeb1
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 21:08:44 +0200

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,14 +2,13 @@
 USE hbtn_0c_0;
 
 -- Prepare a query to retrieve and format the table description
-SELECT
+SELECT 'first_table' AS first_table,
     CONCAT(
-        'first_table',
         'CREATE TABLE `first_table` (',
         '`id` int NOT NULL AUTO_INCREMENT,',
         '`name` varchar(128) DEFAULT NULL,',
         '`c` char(1) DEFAULT NULL,',
         '`created_at` date DEFAULT NULL,',
         'PRIMARY KEY (`id`)',
-        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
-    );
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
+    ) AS description;
