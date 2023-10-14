044a44fc3fcdda9a6bb1b88806c9d4972cb50eaf
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-14 20:29:09 +0200

    First table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,9 +1,11 @@
--- Create the table only if it doesn't exist
+-- Check if the table exists
+SELECT COUNT(id) FROM first_table;
+
+-- If the table doesn't exist, create it and insert a row
 CREATE TABLE IF NOT EXISTS `first_table` (
     `id` INT AUTO_INCREMENT NOT NULL,
     PRIMARY KEY (`id`),
     `name` VARCHAR(256) NOT NULL
 );
 
--- Insert a row to ensure the table exists
-INSERT IGNORE INTO `first_table` (`name`) VALUES ('Sample Data');
+INSERT INTO `first_table` (`name`) VALUES ('Sample Data');
